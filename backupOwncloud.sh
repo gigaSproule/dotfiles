9c8f9988aa6d074f76bf54c8fa5efda181a93e12
gigaSproule <benjamin@benjaminsproule.com>
2014-04-16 21:27:00 +0100

    Added backup own cloud script Updated backup mail script

--- backupOwncloud.sh
+++ backupOwncloud.sh
@@ -6,5 +6,5 @@ backup=$3
 credentials=$4
 
 sudo mount -t cifs //$host/$share $backup -o credentials=$credentials
-sudo tar -zcvf $backup/Maildir.tar.gz $HOME/Maildir
+sudo tar -zcvf $backup/owncloud.tar.gz /var/www/owncloud
 sudo umount $backup
