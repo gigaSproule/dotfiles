c54cfff0764764d49b47b55177826419b86763e4
gigaSproule <benjamin@benjaminsproule.com>
2017-12-10 23:56:59 +0000

    More work to get it working with Kubuntu

--- Kubuntu.py
+++ Kubuntu.py
@@ -5,3 +5,9 @@ class Kubuntu(Ubuntu):
     def __init__(self):
         super().__init__()
 
+
+    def set_development_shortcuts(self):
+        print('Remove setting for alt + mouse dragging moving window')
+        print('Remove keyboard shortcuts under Navigation for ctrl + alt + left/right')
+        print('Remove keyboard shortcut under System for ctrl + alt + l')
+        print('Remove keyboard shortcuts under Windows for ctrl + alt + s, alt + f7')
